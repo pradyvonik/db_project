e22aad5d7117653a6ba21a14757806d8d13a281e
pradyvonik <p.radyvonik@dev-branch.com>
2022-07-19 15:53:15 +0300

    5077 - created subtheme

---
+++ web/themes/custom_olivero_db/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const miniCss = require('mini-css-extract-plugin');
+module.exports = {
+  entry: {
+    dscss: ['./src/index.js'],
+//  javascript: ['./src/Drupal.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test:/\.(s*)css$/,
+      use: [
+        miniCss.loader,
+        'css-loader',
+        'sass-loader',
+      ]
+    }]
+  },
+  plugins: [
+    new miniCss({
+      filename: 'style.css',
+    }),
+  ]
+};
