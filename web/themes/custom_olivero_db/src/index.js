724bee52f1d3e9a4aa61e8af9848df0a6b691176
pradyvonik <p.radyvonik@dev-branch.com>
2022-07-27 19:19:01 +0300

    5144 - themed the Top news block for mobile

--- web/themes/custom_olivero_db/src/index.js
+++ web/themes/custom_olivero_db/src/index.js
@@ -2,10 +2,11 @@
 scss
  */
 import './scss/_libs.scss';
+import './scss/main.scss';
 import './scss/regions/_regions.scss';
 import './scss/header/_header.scss';
 import './scss/header/_menu.scss';
 import './scss/header/_search.scss';
 import './scss/front-page/_content.scss';
-import './scss/main.scss';
+import './scss/front-page/_highlights.scss';
 
