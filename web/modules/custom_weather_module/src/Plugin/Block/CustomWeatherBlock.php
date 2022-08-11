bd2ff84fba8e798b02dbfd46040e1794c3fd557b
paul <p.radyvonik@dev-branch.com>
2022-08-11 10:53:00 +0300

    #5255 - added weather API and display on the site

--- web/modules/custom_weather_module/src/Plugin/Block/CustomWeatherBlock.php
+++ web/modules/custom_weather_module/src/Plugin/Block/CustomWeatherBlock.php
@@ -3,6 +3,7 @@
 namespace Drupal\custom_weather_module\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use GuzzleHttp\Client;
 
 /**
  * Provides a "Custom weather module" block.
@@ -20,8 +21,42 @@ class CustomWeatherBlock extends BlockBase {
    */
   public function build() {
     return [
-      '#markup' => 'Lutsk is the city',
+      '#markup' => 'Your location is: ' . $this->userLocation() . '. The weather is: ' . $this->userWeather(),
     ];
+  }
+
+  /**
+   * Private function for getting user IP.
+   */
+  private function userLocation() {
+    // $user_ip = \Drupal::request()->getClientIp();
+    // @todo fix 2 problems here:
+    // 1. rewrite $user_ip request with dependency injection
+    // 2. make the function get proper user IP that could be geo located
+    // random IP from the Netherlands is being used down here
+    $user_ip = '50.7.93.84';
+    $client = new Client();
+    $request = $client->get('http://ip-api.com/json/' . $user_ip);
+    $response = json_decode($request->getBody(), TRUE);
+    if ($response['status'] == 'success') {
+      $location = $response['city'] . "," . $response['country'];
+    }
+    else {
+      $location = 'Lutsk,Ukraine';
+    }
+    return $location;
+  }
+
+  /**
+   * Private function that gets weather info from weatherapi.com.
+   */
+  private function userWeather() {
+    $api_key = '1f5e3134f1dd43b9bfc150946221008';
+    $location = $this->userLocation();
+    $client = new Client();
+    $request = $client->get('http://api.weatherapi.com/v1/current.json?key=' . $api_key . '&q=' . $location);
+    $response = json_decode($request->getBody(), TRUE);
+    return $response['current']['temp_c'] . ' °C, ' . $response['current']['condition']['text'];
   }
 
 }
